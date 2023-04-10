93f53cc22819f0237312d066be4d46d176506793
Gaurav Shrestha <gauravshresthh@gmail.com>
2023-04-11 05:32:55 +0545

    [feat] initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button, Card, ConfigProvider, theme } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './globalStyles.css';
+
+const App = () => {
+	const [isDarkMode, setIsDarkMode] = useState(false);
+	const { defaultAlgorithm, darkAlgorithm } = theme;
+	const handleClick = () => {
+		setIsDarkMode(previousValue => !previousValue);
+	};
+	return (
+		<ConfigProvider
+			theme={{
+				token: {
+					// colorPrimary: colors.primary,
+				},
+				algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+			}}>
+			<div className="app">
+				<Card style={{ width: 'max-content' }}>
+					<Button onClick={handleClick}>
+						Change Theme to {isDarkMode ? 'Light' : 'Dark'}
+					</Button>
+				</Card>
+			</div>
+		</ConfigProvider>
+	);
+};
 
 export default App;
---
+++ src/theme/metrics.tsx
@@ -0,0 +1,3 @@
+export const margin = {
+	primary: '15px 5px',
+};
